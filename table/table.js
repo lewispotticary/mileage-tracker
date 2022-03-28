2e0f1047dd2b4e5aa104fe267be649e4fd14660f
lewispotticary <lewispotticary@gmail.com>
2022-03-28 21:19:24 +0100

    Added onclick function to edit buttons in table

--- table/table.js
+++ table/table.js
@@ -1,10 +1,18 @@
 import { startPlace } from "../calculateDistance/calculateDistance.js";
 import { destinationPlace } from "../calculateDistance/calculateDistance.js";
-import { traveledMiles } from "../calculateDistance/calculateDistance.js";
+import { milesTraveled } from "../calculateDistance/calculateDistance.js";
+import { nameInput } from "../calculateDistance/calculateDistance.js";
+import { dateInput } from "../calculateDistance/calculateDistance.js";
+
+var tableArray = [];
+var arrayLength;
+var editButton;
 
 export function addTable(traveledMiles){
 
-    console.log(traveledMiles);
+    arrayLength = tableArray.length;
+
+    console.log(arrayLength);
 
     var table = document.getElementById("table");
 
@@ -13,18 +21,56 @@ export function addTable(traveledMiles){
 
     // Insert a cell in the row at index 0
     var nameCell = newRow.insertCell(0);
+    nameCell.setAttribute("class", "tableRow" + arrayLength);
     var dateCell = newRow.insertCell(1);
+    dateCell.setAttribute("class", "tableRow" + arrayLength);
     var locationsCell = newRow.insertCell(2);
+    locationsCell.setAttribute("class", "tableRow" + arrayLength);
     var distanceCell = newRow.insertCell(3);
+    distanceCell.setAttribute("class", "tableRow" + arrayLength);
+    var editCell = newRow.insertCell(4);
 
     // Append a text node to the cell
 
-    var nameText = document.createTextNode(document.getElementById("name-input").value);
+    var nameText = document.createTextNode(nameInput.value);
     nameCell.appendChild(nameText);
-    var dateText = document.createTextNode(document.getElementById("date-input").value);
+
+    var dateText = document.createTextNode(dateInput.value);
     dateCell.appendChild(dateText);
+
     var locationText = document.createTextNode(startPlace.name + " to " + destinationPlace.name);
     locationsCell.appendChild(locationText);
-    var distanceText = document.createTextNode(traveledMiles);
+
+    var distanceText = document.createTextNode(milesTraveled);
     distanceCell.appendChild(distanceText);
+    
+    var editButton = document.createElement("BUTTON");
+    editButton.setAttribute("id", "edit-button" + arrayLength);
+    editButton.setAttribute("class", "edit-button");
+    var editIcon = document.createElement("i");
+    editIcon.className = "fa-solid fa-pen-to-square";
+    editButton.append(editIcon);
+    editCell.appendChild(editButton);
+
+    tableArray.push([nameInput.value, dateInput.value, startPlace.name, destinationPlace.name]);
+
+    editButton = document.getElementsByClassName("edit-button");
+    for(var i=0; i<editButton.length; i++){
+        editButton[i].addEventListener("click", editTable)
+    }
 }
+
+function editTable(){
+    var rowIndex = this.id.slice(-1);
+    console.log("tableRow" + rowIndex);
+    for(var x=0; x<4; x++){
+        var tableRow = document.getElementsByClassName("tableRow" + rowIndex)[x];
+        tableRow.setAttribute('contenteditable', true);
+        tableRow.style.borderColor = "blue";
+        tableRow.click();
+    }
+}
+
+
+
+
