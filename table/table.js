a502d3f793b3653fc26d46413d79357e9164e506
lewispotticary <lewispotticary@gmail.com>
2022-04-10 15:14:45 +0100

    Update table when record deleted function

--- table/table.js
+++ table/table.js
@@ -14,9 +14,6 @@ export function addTable(){
 
     arrayLength = tableArray.length;
 
-    console.log(tableArray);
-    console.log("Array Length:" + arrayLength);
-
     var table = document.getElementById("table");
 
     // Insert a row at the end of the table
@@ -82,7 +79,7 @@ export function addTable(){
     deleteButton.append(deleteIcon);
     deleteCell.append(deleteButton);
 
-    tableArray.push([nameInput.value, dateInput.value, startPlace.name, destinationPlace.name]);
+    tableArray.push([nameInput.value, dateInput.value, startPlace.name, destinationPlace.name, milesTraveled]);
 
     deleteButton = document.getElementsByClassName("delete-button");
 
@@ -90,8 +87,8 @@ export function addTable(){
         deleteButton[x].addEventListener("click", deleteRecord)
     }
 
-
     editButton = document.getElementsByClassName("edit-button");
+
     for(var i=0; i<editButton.length; i++){
         editButton[i].addEventListener("click", editTable)
     }
@@ -117,7 +114,6 @@ function editTable(){
 }
 
 function confirmChanges(){
-    console.log(this.id);
     var rowIndex = this.id.slice(-1);
     for(var i=0; i<4; i++){
         var tableRow = document.getElementsByClassName("tableRow" + rowIndex)[i];
@@ -127,13 +123,92 @@ function confirmChanges(){
     }
     editButton[rowIndex].style.display = "block";
     confirmButton[rowIndex].style.display = "none";
-    console.log(tableArray);
 }
 
 function deleteRecord(){
     var rowIndex = this.id.slice(-1);
     tableArray.splice(rowIndex,1);
-    console.log(tableArray);
-    console.log(document.getElementsByClassName("tableRow" + rowIndex)[0]);
-    document.getElementById("table").deleteRow(+rowIndex + +1);  
+    updateTable();
+}
+
+function updateTable(){
+    for(var i=tableArray.length + 1; i>0; i--){
+        document.getElementById("table").deleteRow(i);  
+    }
+    
+    for(var x=0; x<tableArray.length; x++){ 
+            
+            // Insert a row at the end of the table
+            var newRow = table.insertRow(-1);
+
+            // Insert a cell in the row at index 0
+            var nameCell = newRow.insertCell(0);
+            nameCell.setAttribute("class", "tableRow" + x);
+            var dateCell = newRow.insertCell(1);
+            dateCell.setAttribute("class", "tableRow" + x);
+            var locationsCell = newRow.insertCell(2);
+            locationsCell.setAttribute("class", "tableRow" + x);
+            var distanceCell = newRow.insertCell(3);
+            distanceCell.setAttribute("class", "tableRow" + x);
+            var editCell = newRow.insertCell(4);
+            var deleteCell = newRow.insertCell(5);
+
+            var nameText = document.createTextNode(tableArray[x][0]);
+            nameCell.appendChild(nameText);
+
+            var dateText = document.createTextNode(tableArray[x][1]);
+            dateCell.appendChild(dateText);
+
+            var locationText = document.createTextNode(tableArray[x][2] + " to " + tableArray[x][3]);
+            locationsCell.appendChild(locationText);
+
+            var distanceText = document.createTextNode(tableArray[x][4]);
+            distanceCell.appendChild(distanceText);
+
+            //Create edit button
+    
+            editButton = document.createElement("BUTTON");
+            editButton.setAttribute("id", "edit-button" + x);
+            editButton.setAttribute("class", "edit-button");
+            editButton.title = "Edit";
+            var editIcon = document.createElement("i");
+            editIcon.className = "fa-solid fa-pen-to-square";
+            editButton.append(editIcon);
+            editCell.appendChild(editButton);
+
+            //Create confirm button
+
+            confirmButton = document.createElement("BUTTON");
+            confirmButton.setAttribute("id", "confirm-button" + x);
+            confirmButton.setAttribute("class", "confirm-button");
+            confirmButton.title = "Confirm Changes";
+            var confrimIcon = document.createElement("i");
+            confrimIcon.className = "fa-solid fa-check";
+            confirmButton.append(confrimIcon);
+            editCell.append(confirmButton);
+            confirmButton.style.display = "none";
+
+            //Create delete button
+
+            deleteButton = document.createElement("BUTTON");
+            deleteButton.setAttribute("id", "delete-button" + x);
+            deleteButton.setAttribute("class", "delete-button");
+            deleteButton.title = "Delete";
+            var deleteIcon = document.createElement("i")
+            deleteIcon.className = "fa-solid fa-trash";
+            deleteButton.append(deleteIcon);
+            deleteCell.append(deleteButton);
+
+            deleteButton = document.getElementsByClassName("delete-button");
+
+            for(var y=0; y<deleteButton.length; y++){
+                deleteButton[y].addEventListener("click", deleteRecord)
+            }
+
+            editButton = document.getElementsByClassName("edit-button");
+
+            for(var z=0; z<editButton.length; z++){
+                editButton[z].addEventListener("click", editTable)
+            }      
+    }   
 }
