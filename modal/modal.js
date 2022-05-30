bdfacf76746b39c29d0426e3f4fea0940cc06171
lewispotticary <lewispotticary@gmail.com>
2022-05-30 12:41:45 +0100

    Created signup and login modal

--- modal/modal.js
+++ modal/modal.js
@@ -1,25 +1,42 @@
 // Get the modal
-var modal = document.getElementById("login-modal");
+var loginModal = document.getElementById("login-modal");
+var signupModal = document.getElementById("signup-modal");
 
 // Get the button that opens the modal
 var loginBtn = document.getElementById("loginButton");
+var signupBtn = document.getElementById("signupButton");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("login-close")[0];
+var loginSpan = document.getElementsByClassName("login-close")[0];
+var signupSpan = document.getElementsByClassName("signup-close")[0];
 
 // When the user clicks on the button, open the modal
+
 loginBtn.onclick = function() {
-  modal.style.display = "block";
+  loginModal.style.display = "block";
+}
+
+signupBtn.onclick = function() {
+  signupModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+loginSpan.onclick = function() {
+  loginModal.style.display = "none";
+}
+
+signupSpan.onclick = function() {
+  signupModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
+
+
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == signupModal) {
+    signupModal.style.display = "none";
+  }
+  else if(event.target == loginModal){
+    loginModal.style.display = "none";
   }
 }
