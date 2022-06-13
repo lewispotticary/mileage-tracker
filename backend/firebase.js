c0c414f8266951e1e36fc3543e09ab73a269af03
lewispotticary <lewispotticary@gmail.com>
2022-06-13 23:02:05 +0100

    Added sign in and logout function

--- backend/firebase.js
+++ backend/firebase.js
@@ -23,6 +23,20 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Auth
 const auth = getAuth();
 
+var userID;
+var username;
+const loginButton = document.getElementById("loginButton");
+const signupButton = document.getElementById("signupButton");
+
+onAuthStateChanged(auth, (user) => {
+    if(user){
+        userID = user.uid;
+        username = auth.currentUser.displayName;
+        loginButton.style.display = 'none';
+        signupButton.style.display = 'none';
+    }
+});
+
 var signup = document.getElementsByClassName("signup")[0];
 var login = document.getElementsByClassName("login")[0];
 
@@ -45,16 +59,44 @@ signupForm.addEventListener('submit', (e) => {
       //username = auth.currentUser.displayName;
     })
     .catch((err) => {
-      console.log("error");
+        console.log(err.message)
     })
 })
 
-var userID;
-var username;
+//Sign In Function
 
-onAuthStateChanged(auth, (user) => {
-      userID = user.uid;
-      username = auth.currentUser.displayName;
-      console.log(userID);
-      console.log(username);
-});
+const loginForm = document.querySelector('.login')
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  var email = loginForm.email.value;
+  var password = loginForm.password.value;
+  var loginError = document.getElementById("login-error");
+
+  loginError.innerHTML = "";
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      loginForm.reset();
+    })
+    .catch((err) => {
+        console.log(err.message)
+    })  
+})
+
+//Log Out Function
+
+const logoutButton = document.getElementById("logoutButton");
+
+console.log(logoutButton);
+
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+  .then(() => {
+    console.log('the user signed out')
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
+})
